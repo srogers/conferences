0f02a417b4c7bd93b59c6d53ad68259316159c57
Steven Rogers <srogers1@gmail.com>
2018-07-09 23:34:31 -0500

    Add some publication formats

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -5,13 +5,17 @@ class Publication < ApplicationRecord
 
   validates :presentation_id, presence: true
 
-  TAPE   = 'Tape'
-  CD     = 'CD'
-  VHS    = 'VHS'
-  DISK   = 'DVD/Blu-ray'
-  ONLINE = 'Online'
-  ESTORE = 'e-Store'
-  FORMATS = [TAPE, CD, VHS, DISK, ONLINE, ESTORE]
+  # These are just short word strings and not icons because there aren't good icons for making things like DVD and CD distinct.
+  TAPE    = 'Tape'
+  CD      = 'CD'
+  VHS     = 'VHS'
+  DISK    = 'DVD/Blu-ray'
+  CAMPUS  = 'Campus'
+  YOUTUBE = 'YouTube'      # Is it helpful to make this distinct?
+  PODCAST = 'Podcast'
+  ONLINE  = 'Online'       # Meant to be an "other" catch-all
+  ESTORE  = 'e-Store'      # This is going away . . .
+  FORMATS = [TAPE, CD, VHS, DISK, CAMPUS, YOUTUBE, PODCAST, ONLINE, ESTORE]
 
   validates :format, inclusion: { in: FORMATS, message: "%{value} is not a recognized format" }
 
