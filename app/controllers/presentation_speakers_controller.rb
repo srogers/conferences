2433b03d73fb77d2191e1baefeb0ad021579f3b6
Steven Rogers <srogers1@gmail.com>
2018-07-15 15:36:54 -0500

    beef up ConferenceUsers and PresentationSpeakers controller specs

--- app/controllers/presentation_speakers_controller.rb
+++ app/controllers/presentation_speakers_controller.rb
@@ -3,15 +3,15 @@ class PresentationSpeakersController < ApplicationController
   before_action :require_user
 
   def create
-    presentation_speaker = PresentationSpeaker.new presentation_speaker_params
-    presentation_speaker.creator_id = current_user.id
-    if presentation_speaker.save
-      redirect_to manage_speakers_presentation_path(presentation_speaker.presentation_id)
+    @presentation_speaker = PresentationSpeaker.new presentation_speaker_params
+    @presentation_speaker.creator_id = current_user.id
+    if @presentation_speaker.save
+      redirect_to manage_speakers_presentation_path(@presentation_speaker.presentation_id)
     else
       flash[:error] = 'The speaker/presentation association could not be saved.'
       logger.debug "Presentation Speaker save failed: #{ presentation_speaker.errors.full_messages }"
-      if presentation_speaker&.presentation_id
-        redirect_to manage_speakers_presentation_path(presentation_speaker.presentation_id)
+      if @presentation_speaker&.presentation_id
+        redirect_to manage_speakers_presentation_path(@presentation_speaker.presentation_id)
       else
         redirect_to presentations_path
       end
@@ -19,11 +19,11 @@ class PresentationSpeakersController < ApplicationController
   end
 
   def destroy
-    presentation_speaker = PresentationSpeaker.find(params[:id])
-    if presentation_speaker
-      presentation_id = presentation_speaker.presentation_id
-      presentation_speaker.destroy
-      redirect_to manage_speakers_presentation_path(presentation_speaker.presentation_id)
+    @presentation_speaker = PresentationSpeaker.find(params[:id])
+    if @presentation_speaker
+      presentation_id = @presentation_speaker.presentation_id
+      @presentation_speaker.destroy
+      redirect_to manage_speakers_presentation_path(presentation_id)
     else
       render body: nil
     end
--- spec/controllers/presentation_speakers_controller_spec.rb
+++ spec/controllers/presentation_speakers_controller_spec.rb
@@ -12,16 +12,33 @@ RSpec.describe PresentationSpeakersController, type: :controller do
       post :create, params: { presentation_speaker: { presentation_id: 1, speaker_id: 1 }}
     end
 
-    it "sets the creator"
-    it "saves the conference speaker"
+    it "sets the creator" do
+      expect(assigns(:presentation_speaker).creator_id).to eq(@current_user.id)
+    end
 
-    it "redirects to the conference show page" do
-      expect(response).to redirect_to presentation_path(1)
+    it "saves the presentation speaker" do
+      expect(assigns(:presentation_speaker).errors).to be_empty
+      expect(assigns(:presentation_speaker).id).to be_present
+    end
+
+    it "redirects to the manage_speakers page for the presentation" do
+      expect(response).to redirect_to manage_speakers_presentation_path(1)
     end
   end
 
   describe "DELETE #destroy" do
-    it "destroys the conference/speaker relationship"
+    before do
+      @presentation_speaker = create :presentation_speaker
+      delete :destroy, params: {id: @presentation_speaker.to_param}
+    end
+
+    it "finds the presentation/speaker relationship" do
+      expect(assigns(:presentation_speaker)).to eq(@presentation_speaker)
+    end
+
+    it "destroys the presentation/speaker relationship" do
+      expect { @presentation_speaker.reload }.to raise_error ActiveRecord::RecordNotFound
+    end
   end
 
 end
