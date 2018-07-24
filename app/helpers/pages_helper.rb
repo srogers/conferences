98f511c5cf202617a94614cf93f438fbf908b41c
Steven Rogers <srogers1@gmail.com>
2018-07-24 13:49:37 -0500

    #51 Upgrade the menus, fix some legacy styles, and change a few spacings so the layout generally works again - colors still weird.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,6 @@
 module PagesHelper
 
-  def menu_attributes(current_tab)
-    attrs = { role: "presentation"} # a foo-foo attribute all bootstrap menu items get
-    attrs = attrs.merge(class: "active") if current_tab == action_name
-    return attrs
+  def tab_active?(current_tab)
+    current_tab == action_name ? ' active' : ''
   end
 end
