b63ad485a6b1bb994ed8e3c387b322f8759f6305
Steven Rogers <srogers1@gmail.com>
2018-07-27 21:42:35 -0500

    Add a potentially necessary parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -130,6 +130,6 @@ class UsersController < ApplicationController
 
   def users_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :role_id, :city, :state, :country,
-                                 :active, :approved, :role_id, :photo, :remove_photo, :time_zone, :show_attendance)
+                                 :active, :approved, :role_id, :photo, :remove_photo, :time_zone, :show_attendance, :show_contributor)
   end
 end
