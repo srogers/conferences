f57d30441cb8b767048e073052d2a23de3f4fe35
Steven Rogers <srogers1@gmail.com>
2018-08-03 15:30:12 -0500

    #15 Fix a bug that makes generated docs always PDF

--- app/models/document.rb
+++ app/models/document.rb
@@ -21,7 +21,7 @@ class Document < ApplicationRecord
 
   def initialize_values
     self.status = PENDING
-    self.format = PDF
+    self.format = PDF if format.blank?
     # set a default option if nothing is set
     self.conferences = true if options.map{|k,v| k.to_s if v}.compact.empty?
     # Set the name based on the options - what will be in the file
