76242c12cfbee6fd6a1c0296adbbcb330f3580c6
Steven Rogers <srogers1@gmail.com>
2018-09-04 18:42:39 -0500

    #131 Fix a bug that causes redirect after speaker/publication manage - routes have to always use to_param and not the ID

--- app/controllers/presentation_speakers_controller.rb
+++ app/controllers/presentation_speakers_controller.rb
@@ -6,12 +6,12 @@ class PresentationSpeakersController < ApplicationController
     @presentation_speaker = PresentationSpeaker.new presentation_speaker_params
     @presentation_speaker.creator_id = current_user.id
     if @presentation_speaker.save
-      redirect_to manage_speakers_presentation_path(@presentation_speaker.presentation_id)
+      redirect_to manage_speakers_presentation_path(@presentation_speaker.presentation.to_param)
     else
       flash[:error] = 'The speaker/presentation association could not be saved.'
       logger.debug "Presentation Speaker save failed: #{ presentation_speaker.errors.full_messages }"
       if @presentation_speaker&.presentation_id
-        redirect_to manage_speakers_presentation_path(@presentation_speaker.presentation_id)
+        redirect_to manage_speakers_presentation_path(@presentation_speaker.presentation.to_param)
       else
         redirect_to presentations_path
       end
@@ -21,7 +21,7 @@ class PresentationSpeakersController < ApplicationController
   def destroy
     @presentation_speaker = PresentationSpeaker.find(params[:id])
     if @presentation_speaker
-      presentation_id = @presentation_speaker.presentation_id
+      presentation_id = @presentation_speaker.presentation.to_param
       @presentation_speaker.destroy
       redirect_to manage_speakers_presentation_path(presentation_id)
     else
