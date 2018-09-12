79d23f192ea5db5950bf86b7106be68a77ce32c7
Steven Rogers <srogers1@gmail.com>
2018-09-11 22:16:15 -0500

    Fix a bug in the way Speak sortable_name override works. Allow the admin to override, make sure the override sticks through editor updates.

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -90,9 +90,10 @@ class SpeakersController < ApplicationController
 
   def update
     if @speaker.update_attributes speaker_params
+      @speaker.update_column :sortable_name, params[:speaker][:sortable_name] if params[:update_name] == 'true'
       redirect_to speaker_path(@speaker)
     else
-      flash.now[:error] = 'Your speaker could not be saved.'
+      flash.now[:error] = 'Your changes could not be saved.'
       logger.debug "Speaker save failed: #{ @speaker.errors.full_messages }"
       render 'edit'
     end
