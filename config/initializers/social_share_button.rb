2a54b11c68c1245cab87e2e569bc5c45bf61fbbf
Steven Rogers <srogers1@gmail.com>
2019-09-18 09:48:40 -0500

    Enable social sharing on foreign sites, and even the ones I don't like

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -2,5 +2,5 @@ SocialShareButton.configure do |config|
   # The available sites:
   # Facebook, Twitter, Douban, Google+, Weibo, QZone, Google Bookmark, Delicious, Tumblr, Pinterest, Email, LinkedIn,
   # WeChat (Weixin), Vkontakte, Odnoklassniki, Xing, Reddit, Hacker News, Telegram, WhatsApp
-  config.allow_sites = %w(twitter google_bookmark delicious tumblr reddit email)
+  config.allow_sites = %w(twitter linkedIn Douban Weibo google_bookmark delicious tumblr reddit email)
 end
