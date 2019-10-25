e748485ac18cd0f99fa22e77df88fd2c0a7a123c
Steven Rogers <srogers1@gmail.com>
2019-10-24 23:08:44 -0500

    Add a working cleanup for Cucumber scenarios so Cucumber and Rspec don't interfere with each other

--- features/support/env.rb
+++ features/support/env.rb
@@ -31,7 +31,20 @@ ActionController::Base.allow_rescue = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
-  DatabaseCleaner.strategy = :transaction
+  require 'database_cleaner'
+  require 'database_cleaner/cucumber'
+
+  DatabaseCleaner.strategy = :truncation
+  #DatabaseCleaner.strategy = :transaction
+
+  Before do
+    DatabaseCleaner.start
+  end
+
+  After do |scenario|
+    DatabaseCleaner.clean
+  end
+
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
