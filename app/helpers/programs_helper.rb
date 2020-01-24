cc876a7df6070595bf3c4725e03f6e31fa86bdd2
Steven Rogers <srogers1@gmail.com>
2020-01-24 14:54:34 -0600

    #332 Show the new info icons along with the old program icon in events listing - remove the old icon later in #343

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -1,2 +1,12 @@
 module ProgramsHelper
+
+  # Options:  :link_classes,
+  def linked_icon_for_program(program, options={})
+    if program.url.present?
+      link_to icon('far', 'external-link-alt', class: 'fa-fw'), program.url, target: '_blank', title: program.name, class: options[:link_classes]
+    else
+      link_to icon('far', 'file-pdf', class: 'fa-fw'), download_event_program_path(program.conference, program), target: '_blank', title: program.name, class: options[:link_classes]
+    end
+  end
+
 end
