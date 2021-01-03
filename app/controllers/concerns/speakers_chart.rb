97d02e253347362183d2c2609701506a9d9bd436
Steven Rogers <srogers1@gmail.com>
2021-01-02 21:54:26 -0600

    #441 Standardize speaker queries - quote speaker name in events chart

--- app/controllers/concerns/speakers_chart.rb
+++ app/controllers/concerns/speakers_chart.rb
@@ -3,13 +3,27 @@ module SpeakersChart
 
   include SharedQueries
 
+  # Sets up the SELECT and FROM in the query - this should be the same everywhere, except possibly some edge cases with aggregates.
+  # Pass the results of this into filter_speakerss()
+  def speaker_collection(collection=Speaker)
+    collection.includes(:presentations)
+    if param_context(:search_term).present?
+      collection = collection.references(:presentations => :conference).includes(:presentations => :conference)
+    end
+    return collection
+  end
+
+  # Set up the query but leave it open so all the filter/aggregate methods can share the same query setup.
+  def speaker_query(collection=Speaker)
+    query = init_query(speaker_collection(collection))
+    query = speaker_where(query)
+  end
+
   # The controller index action and the chart-building action share these WHERE clauses for consistency. It includes conference
   # and speakers, which makes searches slightly less efficient, but much more in line with what a user would expect re matches.
-  def filter_speakers(speakers)
-    query = init_query(speakers)
-    query = speaker_where(query)
-
-    speakers.where(query.where_clause, *query.bindings)
+  def filter_speakers(collection)
+    query = speaker_query(collection)
+    query.collection.where(query.where_clause, *query.bindings)
   end
 
   # Builds a hash of speaker counts that looks like: {"Hans Schantz"=>7, "Robert Garmong"=>6, "Ann Ciccolella"=>5, "Yaron Brook"=>5 }
@@ -21,11 +35,10 @@ module SpeakersChart
 
       # Speaker queries don't reference presentations all - just PresentationSpeakers for counting.
       # Conferences are referenced for the "My Conferences" case
-      base = PresentationSpeaker.includes(:speaker, :presentation => :conference)  #.includes(:presentation => { :taggings => :tag }).references(:presentation => { :taggings => :tag })
-      query = init_query base
+      collection = PresentationSpeaker.includes(:speaker, :presentation => :conference)  #.includes(:presentation => { :taggings => :tag }).references(:presentation => { :taggings => :tag })
+      query = init_query collection
       query = speaker_where(query)
-
-      data = base.group("speakers.name").where(query.where_clause, *query.bindings).count(:presentation_id)
+      data = query.collection.group("speakers.name").where(query.where_clause, *query.bindings).count(:presentation_id)
       data = data.sort_by{ |k,v| v }.reverse
 
     # Handles the My Conferences case
@@ -33,12 +46,11 @@ module SpeakersChart
       data = PresentationSpeaker.includes(:speaker, :presentation => :conference).where("conferences.id in (SELECT conference_id FROM conference_users WHERE user_id = ?)", current_user.id).group("speakers.name").order(Arel.sql("count(presentation_id) DESC")).count(:presentation_id)
 
     elsif speaker_slug.present?
-      base = PresentationSpeaker.includes(:speaker, :presentation => :conference)
-      query = init_query(base)
+      collection = PresentationSpeaker.includes(:speaker, :presentation => :conference)
+      query = init_query(collection)
       query = speaker_where(query)
       query = one_speaker_where(query, speaker_slug)
-
-      data = base.group("speakers.name").where(query.where_clause, *query.bindings).count(:presentation_id)
+      data = query.collection.group("speakers.name").where(query.where_clause, *query.bindings).count(:presentation_id)
 
     else
       # Show the top speakers - otherwise it's too big - limit is not great here, because even though results are sorted
@@ -62,12 +74,9 @@ module SpeakersChart
     if param_context(:search_term).present? || param_context(:tag).present?
       # This repeats the WHERE clause from the conferences controller so the the chart results will match the search results.
       # Start with the Speaker class so SharedQueries will build a speaker-based query
-      base = Speaker.includes(:presentations => :conference)
-      query = init_query(base)
-      query = speaker_where(query)
-      # don't match on presentations in this context - "Smith" matches things like "Adam Smith" in  titles - distracting - do that in the presentations tab
-
-      data = base.group("speakers.name").where(query.where_clause, *query.bindings).count('conferences.id')
+      collection = Speaker.includes(:presentations => :conference)
+      query = filter_speakers(collection)
+      data = query.group("speakers.name").count('conferences.id')
       # do the sort in Ruby - don't filter when a search term is present
       data = data.sort_by{ |name, count| count }.reverse.to_h
 
@@ -77,12 +86,11 @@ module SpeakersChart
 
     elsif speaker_slug.present?
       # This happens when the user has drilled down from the speakers list to a specific speaker and presentations.
-      base = Speaker.includes(:presentations => :conference)
-      query = init_query base
-      query = speaker_where(query)
+      collection = Speaker.includes(:presentations => :conference)
+      query = speaker_query(collection)
       query = one_speaker_where(query, speaker_slug)
+      data = query.collection.group("conferences.name").where(query.where_clause, *query.bindings).count('presentations.id')
 
-      data = base.group("conferences.name").where(query.where_clause, *query.bindings).count('presentations.id')
     else
       # Show the top speakers - otherwise it's too big - limit is not great here, because even though results are sorted
       # by count, limit might cut off speakers with the same count as speakers shown, which is misleading.
