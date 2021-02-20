93b6de06a2a82271687f41e007ed364741a620e7
Steven Rogers <srogers1@gmail.com>
2021-02-19 22:06:22 -0600

    SSL now works on Heroku for both fee and paid apps, so force SSL in both staging and production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,12 +45,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
 
 
-  # Force SSL only on production - staging doesn't have a cert
-  if ENV['MAIL_HOST'] == 'objectivistmedia.com'
-    # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-    config.force_ssl = true
-    config.ssl_options = {  redirect: { status: 307, port: 81 } }
-  end
+  # SSL now works on Heroku for both fee and paid apps, so force SSL in both staging and production.
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+  config.ssl_options = {  redirect: { status: 307, port: 81 } }
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
