82731a4662e1bc48b8cdda2b28597100be711d2a
Steven Rogers <srogers1@gmail.com>
2021-11-27 16:53:39 -0600

    #525 turn off traces config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,7 +7,7 @@ Sentry.init do |config|
   # -- New config for new features in sentry-ruby
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
-  config.traces_sample_rate = 1
+  # config.traces_sample_rate = 1
   # or
   # config.traces_sampler = lambda do |context|
   #   true
